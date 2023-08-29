09871d38b5e69b4f4b68733ba839d62dc8aae494
R8nDr0p <rainchrstn@gmail.com>
2023-08-29 18:25:30 +0800

    finished the day function on the watch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const secondsHand = document.getElementById("seconds-hand");
 const minutesHand = document.getElementById("minutes-hand");
 const hoursHand = document.getElementById("hours-hand");
+const dayDial = document.querySelector(".dial-day");
 
 function getTime() {
   const now = new Date();
@@ -8,26 +9,52 @@ function getTime() {
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
+  const day = now.getDay();
 
   const secondsRotation = (seconds + milliSeconds / 1000) * 6;
   const minutesRotation = ((minutes + seconds / 60) / 60) * 360;
   const hoursRotation = ((hours + minutes / 60) / 12) * 360;
+  const degPerDayofWeek = 360 / 7;
+  const currDay = -day * degPerDayofWeek;
+  // console.log(currDay);
 
   secondsHand.style.transform = `rotate(${secondsRotation}deg)`;
   minutesHand.style.transform = `rotate(${minutesRotation}deg)`;
   hoursHand.style.transform = `rotate(${hoursRotation}deg)`;
+  dayDial.style.transform = `translateX(-50%) rotate(${currDay}deg)`;
   // request animation frame para smooth ang rotation ng second hand parang automatic o mechanical watch talga siya
   requestAnimationFrame(getTime);
 }
 
 const weekDay = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
+  {
+    name: "Sunday",
+    rem: 0.15,
+  },
+  {
+    name: "Monday",
+    rem: 0.25,
+  },
+  {
+    name: "Tuesday",
+    rem: 0.5,
+  },
+  {
+    name: "Wednesday",
+    rem: 0.7,
+  },
+  {
+    name: "Thursday",
+    rem: 0.25,
+  },
+  {
+    name: "Friday",
+    rem: 0.5,
+  },
+  {
+    name: "Saturday",
+    rem: 0.25,
+  },
 ];
 
 // getTime();
@@ -35,12 +62,16 @@ const weekDay = [
 weekDay.forEach((day, index) => {
   const div = document.createElement("div");
   div.setAttribute("class", `day day${index}`);
-
-  for (let i = 0; i < day.length; i++) {
+  console.log(day.name);
+  for (let i = 0; i < day.name.length; i++) {
     const span = document.createElement("span");
-    span.textContent = day[i];
-    span.style.position = "inline-block";
-    span.style.transform = "rotate(90deg)";
+    const letter = day.name[i];
+    // console.log(day[i]);
+    span.textContent = letter;
+    span.style.display = "inline-block";
+    span.style.paddingTop = `${day.rem}rem`;
+    const deg = 350 + i * 6;
+    span.style.transform = `rotate(${deg}deg)`;
     div.appendChild(span);
   }
   document.querySelector(".dial-day").appendChild(div);
