f43edc189757cb287bcce29f7ab3f78391e2179a
R8nDr0p <rainchrstn@gmail.com>
2023-09-06 20:12:50 +0800

    Revert "added day of the month feature not yet finish fix the javascript"
    This reverts commit e1d72bab295ca7d546fcf0e9a5f8da5c04f21f41.

--- app.js
+++ app.js
@@ -51,23 +51,24 @@ const weekDay = [
   },
   {
     name: "Thursday",
-    rem: 0.6,
+    rem: 0.25,
   },
   {
     name: "Friday",
-    rem: 0.45,
+    rem: 0.5,
   },
   {
     name: "Saturday",
     rem: 0.25,
   },
 ];
+
 // getTime();
 // setInterval(getTime, 1000);
 weekDay.forEach((day, index) => {
   const div = document.createElement("div");
   div.setAttribute("class", `day day${index}`);
-  // console.log(day.name);
+  console.log(day.name);
   for (let i = 0; i < day.name.length; i++) {
     const span = document.createElement("span");
     const letter = day.name[i];
